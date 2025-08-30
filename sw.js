a81328a51f4975a0b42bcf41a2250641d69f3991
Diogo Barbosa Faleiro <69588292+DiogoFaleiro@users.noreply.github.com>
2025-08-30 14:00:09 -0300

    Adiciona comentários para verificar a existência de arquivos no manifest.json e no Service Worker

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
-        '/index.html',
-        '/style.css',
+        '/index.html', // Verifique se o arquivo está na raiz do seu projeto
+        '/style.css', 
         '/script.js',
         '/assets/favicon-192.png',
         '/assets/favicon-512.png',
         '/assets/logo.png',
-      ]).catch(err => {
-        console.error('Erro ao adicionar ao cache:', err);
-      });
+      ]);
+    }).catch(err => {
+      console.error('Erro ao adicionar ao cache:', err);
     })
   );
 });
